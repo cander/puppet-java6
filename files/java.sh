d03a84e4631d1ef3c079e143331cdd016dfd156e
Charles Anderson <master.sparkle@gmail.com>
2016-03-29 14:18:31 -0700

    Fix java wrapper to respect existing JAVA_HOME
    Only set JAVA_HOME if it isn't already set.  Allows the user to select among
    multiple versions of Java that are installed.

--- files/java.sh
+++ files/java.sh
@@ -5,8 +5,12 @@
 
 export DYLD_FALLBACK_LIBRARY_PATH="$BOXEN_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
-if [ -x /usr/libexec/java_home ]; then
-  export JAVA_HOME=`/usr/libexec/java_home`
+# only set JAVA_HOME if it isn't already set
+if [ -z "$JAVA_HOME" ]
+then
+    if [ -x /usr/libexec/java_home ]; then
+      export JAVA_HOME=`/usr/libexec/java_home`
+    fi
 fi
 
 exec /usr/bin/java "$@"
